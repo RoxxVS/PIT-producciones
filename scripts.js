9301c3969174293022ae0262b72ddcf8c81778b4
Roxana Valeria Solernou <119822642+RoxxVS@users.noreply.github.com>
2024-04-20 17:58:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/**Animación del catálogo*/
 let prod1 = document.getElementById('prod1');
 let prod2 = document.getElementById('prod2');
 let prod3 = document.getElementById('prod3');
@@ -10,33 +11,74 @@ let prod9 = document.getElementById('prod9');
 let prod10 = document.getElementById('prod10');
 let prod11 = document.getElementById('prod11');
 let prod12 = document.getElementById('prod12');
-
+function cambiarUbicacion (prod, ubicacion) {
+    prod.style.gridArea = ubicacion;
+}
 function cambioUno() {
-    prod1.style.gridArea = '1/1/2/2';
-    prod4.style.gridArea = '2/1/4/3';
-    prod6.style.gridArea = '1/3/3/4';
-    prod10.style.gridArea = '5/4/6/5';
-    prod12.style.gridArea = '4/2/6/4';
+    cambiarUbicacion(prod1, '1/1/2/2');
+    cambiarUbicacion(prod2, '1/2/2/3');
+    cambiarUbicacion(prod3, '1/4/2/5');
+    cambiarUbicacion(prod4, '2/1/4/3');
+    cambiarUbicacion(prod5, '2/4/4/5');
+    cambiarUbicacion(prod6, '1/3/3/4');
+    cambiarUbicacion(prod7, '3/3/4/4');
+    cambiarUbicacion(prod8, '4/1/5/2');
+    cambiarUbicacion(prod9, '5/1/6/2');
+    cambiarUbicacion(prod10, '5/4/6/5');
+    cambiarUbicacion(prod11, '4/4/5/5');
+    cambiarUbicacion(prod12, '4/2/6/4');
 }
 function cambioDos() {
-    prod2.style.gridArea = '1/2/3/4';
-    prod4.style.gridArea = '2/1/3/2';
-    prod5.style.gridArea = '4/1/6/2';
-    prod6.style.gridArea = '3/2/5/3';
-    prod8.style.gridArea = '4/3/6/5';
-    prod10.style.gridArea = '3/3/4/4';
-    prod11.style.gridArea = '3/4/4/5';
-    prod12.style.gridArea = '5/2/6/3';
+    cambiarUbicacion(prod1, '1/1/2/2');
+    cambiarUbicacion(prod2, '1/2/3/4');
+    cambiarUbicacion(prod3, '1/4/2/5');
+    cambiarUbicacion(prod4, '2/1/3/2');
+    cambiarUbicacion(prod5, '4/1/6/2');
+    cambiarUbicacion(prod6, '3/2/5/3');
+    cambiarUbicacion(prod7, '2/4/3/5');
+    cambiarUbicacion(prod8, '4/3/6/5');
+    cambiarUbicacion(prod9, '3/1/4/2');
+    cambiarUbicacion(prod10, '3/3/4/4');
+    cambiarUbicacion(prod11, '3/4/4/5');
+    cambiarUbicacion(prod12, '5/2/6/3');
 }
 function cambioTres() {
-    prod1.style.gridArea = '1/1/3/3';
-    prod2.style.gridArea = '1/3/2/4';
-    prod4.style.gridArea = '2/3/3/4';
-    prod5.style.gridArea = '2/4/4/5';
-    prod6.style.gridArea = '3/1/5/2';
-    prod8.style.gridArea = '3/3/4/4';
-    prod10.style.gridArea = '4/3/6/5';
-    prod11.style.gridArea = '5/1/6/2';
+    cambiarUbicacion(prod1, '1/1/3/3');
+    cambiarUbicacion(prod2, '1/3/2/4');
+    cambiarUbicacion(prod3, '1/4/2/5');
+    cambiarUbicacion(prod4, '2/3/3/4');
+    cambiarUbicacion(prod5, '2/4/4/5');
+    cambiarUbicacion(prod6, '3/1/5/2');
+    cambiarUbicacion(prod7, '3/2/4/3');
+    cambiarUbicacion(prod8, '3/3/4/4');
+    cambiarUbicacion(prod9, '4/2/5/3');
+    cambiarUbicacion(prod10, '4/3/6/5');
+    cambiarUbicacion(prod11, '5/1/6/2');
+    cambiarUbicacion(prod12, '5/2/6/3');
+}
+function cambiosTodos() {
+setTimeout(cambioUno, 1500);
+setTimeout(cambioDos, 3000);
+setTimeout(cambioTres, 4500);
+setTimeout(cambioUno, 6000);
+}
+function bucle() {
+    setInterval(cambiosTodos, 6000);
+}
+bucle();
+/**Esconder/Mostrar historia */
+let botonMostrar = document.getElementById('mostrarTexto');
+let botonEsconder = document.getElementById('esconderTexto');
+let textoOculto = document.getElementById('textoOculto');
+botonMostrar.addEventListener('click', mostrarTextoOculto);
+function mostrarTextoOculto() {
+    textoOculto.style.display = 'block';
+    botonMostrar.style.display = 'none';
+}
+botonEsconder.addEventListener('click', esconderTextoOculto);
+function esconderTextoOculto() {
+    textoOculto.style.display = 'none';
+    botonMostrar.style.display = 'inline-block';
 }
 
 
